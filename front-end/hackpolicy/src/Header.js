a0c485131bb47eb380e9b9101616f92a3cdedf51
Jeewoo Chung <awesomejwc@icloud.com>
2021-03-27 13:02:34 -0700

    Update Header.js

--- front-end/hackpolicy/src/Header.js
+++ front-end/hackpolicy/src/Header.js
@@ -1,11 +1,29 @@
-import { AppBar, Toolbar } from '@material-ui/core'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles,
+} from '@material-ui/core'
 import './App.css'
 
+const useStyle = makeStyles({
+  titleText: {
+    margin: '10px',
+  },
+})
+
 const Header = () => {
+  const classes = useStyle()
   return (
     <AppBar position="static">
       <Toolbar>
-        <img src="./logo.svg" alt="Logo" height="25" width="25" />
+        <IconButton aria-label="AppIcon">
+          <img src="https://svgur.com/i/VVY.svg" alt="Logo" width="50" />
+        </IconButton>
+        <Typography variant="h4" className={classes.titleText}>
+          Viridi
+        </Typography>
       </Toolbar>
     </AppBar>
   )
